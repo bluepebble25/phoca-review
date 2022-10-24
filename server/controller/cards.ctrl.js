7cc87695a7be98deb20b0d2a0754159421dbe151
bluepebble25 <bluepebble25@gmail.com>
2022-10-24 21:16:28 +0900

    #1 feat: 카드 create API 추가

--- server/controller/cards.ctrl.js
+++ server/controller/cards.ctrl.js
@@ -1,19 +1,40 @@
 const Ajv = require('ajv');
 const ajv = new Ajv();
+const fs = require('fs');
+
 const { CardSchema } = require('../models/Card');
 
+// controllers
 const getAllCards = (req, res) => {
+  console.log(fs.existsSync('uploads'));
   res.send('hi');
 };
 
 const getCard = (req, res) => {};
 
 const createCard = (req, res) => {
-  console.log(req.body);
-  console.log(req.body.front.image.translate);
-  const isValid = ajv.validate(CardSchema, req.body);
+  const cardInfo = JSON.parse(req.body.jsonObject);
+  const isValid = ajv.validate(CardSchema, cardInfo);
+
+  console.log(JSON.parse(req.body.jsonObject));
+  console.log(req.files);
   console.log(isValid);
-  res.status(200).end();
+
+  if (!isValid) {
+    res.status(400).end();
+  } else {
+    fs.writeFile(
+      `uploads/card_info/${cardInfo.title}.json`,
+      req.body.jsonObject,
+      function (err) {
+        if (err === null) {
+          res.send('upload success.');
+        } else {
+          res.send('upload fail');
+        }
+      }
+    );
+  }
 };
 
 const updateCard = (req, res) => {};
--- server/router/cards.js
+++ server/router/cards.js
@@ -1,10 +1,38 @@
 const express = require('express');
 const router = express.Router();
 const ctrl = require('../controller/cards.ctrl');
+const fs = require('fs');
+
+const multer = require('multer');
+
+function mkdir(path) {
+  const exists = fs.existsSync(path);
+  if (!exists) {
+    fs.mkdirSync(path, { recursive: true });
+  }
+}
+
+mkdir('uploads/images');
+mkdir('uploads/card_info');
+
+let storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  },
+  fileFilter: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    cb(null, true);
+  },
+});
+
+const upload = multer({ storage: storage }).array('file');
 
 router.get('/', ctrl.getAllCards);
 router.get('/:id', ctrl.getCard);
-router.post('/', ctrl.createCard);
+router.post('/', upload, ctrl.createCard);
 router.put('/:id', ctrl.updateCard);
 router.delete('/:id', ctrl.deleteCard);
 
