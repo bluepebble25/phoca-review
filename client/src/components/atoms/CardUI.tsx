09cc3649d970e4c967e04e0e676404688d82aaec
bluepebble25 <bluepebble25@gmail.com>
2023-01-16 20:04:46 +0900

    #13 feat: 카드 배경색 (단색, 그라디언트) 렌더링되게 함

--- client/src/components/atoms/CardUI.tsx
+++ client/src/components/atoms/CardUI.tsx
@@ -1,4 +1,6 @@
 import { css } from '@emotion/react';
+import { useEffect, useState } from 'react';
+import { colorPalette, gradient } from '../../_lib/styles/colorPalette';
 
 interface Props {
   title: string;
@@ -12,11 +14,40 @@ interface Props {
   };
 }
 
+interface bgStyleProps {
+  value: string;
+  type: 'color' | 'gradient' | 'none';
+}
+
 function CardUI({ title, content, author, image, background }: Props) {
+  const [bgStyle, setBgStyle] = useState<bgStyleProps>({
+    value: '',
+    type: 'none',
+  });
+
+  useEffect(() => {
+    const getHexColor = (color: string) => {
+      let hexColorCode = color;
+      if (color.split('')[0] !== '#') {
+        hexColorCode = colorPalette[color];
+      }
+      return hexColorCode;
+    };
+
+    if (background && background.color) {
+      setBgStyle({ value: getHexColor(background.color), type: 'color' });
+    } else if (background && background.gradient) {
+      setBgStyle({ value: gradient[background.gradient], type: 'gradient' });
+    }
+  }, []);
+  if (background && background.gradient) {
+    console.log(title, background && background.gradient, bgStyle);
+  }
+
   return (
     <div css={cardUIStyle}>
       <img src={image} alt="" css={imgStyle} />
-      <div css={contentStyle}>
+      <div css={contentStyle(bgStyle)}>
         <h2 css={titleStyle}>{title}</h2>
         <p css={authorStyle}>{author}</p>
       </div>
@@ -40,10 +71,12 @@ const imgStyle = css`
   object-fit: cover;
 `;
 
-const contentStyle = css`
+const contentStyle = ({ value, type }: bgStyleProps) => css`
   position: absolute;
   width: 100%;
   height: 100%;
+  ${type === 'color' && 'background-color:' + value + ';'}
+  ${type === 'gradient' && 'background:' + value + ';'}
 `;
 
 const titleStyle = css`
