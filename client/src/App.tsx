547bd7a1b09211d28ec46ef3ea808d2076b90413
bluepebble25 <bluepebble25@gmail.com>
2023-02-17 16:01:33 +0900

    #28 feat: 페이지 전환 시 fade-in 되는 애니메이션 효과 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route, Navigate, Outlet } from 'react-router-dom';
+import { AnimatePresence, motion } from 'framer-motion';
 import BookPage from './pages/BookPage';
 import CardDetailPage from './pages/CardDetailPage';
 import CreateCardPage from './pages/CreateCardPage';
@@ -7,25 +8,54 @@ import Nav from './components/organisms/Nav';
 
 const MainLayout = () => {
   return (
-    <>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.6 }}
+    >
       <Nav />
       <Outlet />
-    </>
+    </motion.div>
+  );
+};
+
+interface createCardPageWraperProps {
+  isEditPage: boolean;
+}
+const CreateCardPageWrapper = ({ isEditPage }: createCardPageWraperProps) => {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.8 }}
+    >
+      <CreateCardPage isEditPage={isEditPage} />
+    </motion.div>
   );
 };
 
 function App() {
   return (
-    <Routes>
-      <Route element={<MainLayout />}>
-        <Route index element={<Navigate to="/cards" />} />
-        <Route path="/cards" element={<BookPage />}>
-          <Route path="/cards/:id" element={<CardDetailPage />} />
+    <AnimatePresence>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route index element={<Navigate to="/cards" />} />
+          <Route path="/cards" element={<BookPage />}>
+            <Route path="/cards/:id" element={<CardDetailPage />} />
+          </Route>
         </Route>
-      </Route>
-      <Route path="/create" element={<CreateCardPage />} />
-      <Route path="/edit/cards/:id" element={<CreateCardPage isEditPage />} />
-    </Routes>
+        <Route
+          path="/create"
+          element={<CreateCardPageWrapper isEditPage={false} />}
+        />
+        <Route
+          path="/edit/cards/:id"
+          element={<CreateCardPageWrapper isEditPage={true} />}
+        />
+      </Routes>
+    </AnimatePresence>
   );
 }
 
