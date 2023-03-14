34dab7ed10d02f48c71026ce48c9d237a88c6670
bluepebble25 <bluepebble25@gmail.com>
2023-03-14 14:47:07 +0900

    deploy: React build 파일 서버에 올리는 코드 추가

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const cors = require('cors');
 
 const cards = require('./router/cards');
@@ -17,9 +18,14 @@ app.use(
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use('/uploads', express.static('uploads')); // use '/uploads' folder as a static file provider
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 // Routing
 app.use('/api/cards', cards);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server is running on ${port}`);
